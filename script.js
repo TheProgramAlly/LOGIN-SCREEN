1dd1b06de37a4449519be2ae5a2a7c39ecf0e0be
Allyson <allyson.luciano66@gmail.com>
2024-01-16 23:43:22 -0300

    button enter js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const inputs = document.querySelectorAll('.input');
 
+const button = document.querySelector('.login__button');
+
+
 const handleFocus = ({ target } ) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
@@ -14,10 +17,23 @@ if (target.value === '') {
 
 }
 
-    
+   
 }
+
+const handleChange = () => {
+    const [username, password]= inputs;
+
+    if (username.value && password.value.length >= 8) {
+
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled' , '');
+    }
+ }
+
 
 
 
 inputs.forEach((input) => input.addEventListener('focus' , handleFocus));
 inputs.forEach((input) => input.addEventListener('focusout' , handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input' , handleChange));
